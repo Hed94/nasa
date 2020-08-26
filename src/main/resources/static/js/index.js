643a2077521fb1d1f96f674436ac5ee86be638bb
Hed94 <Hed94@seznam.cz>
2020-08-26 10:47:09 +0200

    Highlight button

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -12,7 +12,7 @@ alert(this.text)
 */
 
 // Množný způsob volání tlačítek místo individuálního způsobu výše
-// Teoreticky by šlo používat jen id, kdyby se z toho jejich ID smazala marsApi
+// Teoreticky by šlo používat jen id, kdyby se z toho jejich ID smazala marsApi ale takhle je to víc dynamické
 let marsApiButtons = document.querySelectorAll("button[id*='marsApi']")
 marsApiButtons.forEach(button => button.addEventListener('click',function(){
 const buttonId = this.id;
@@ -21,3 +21,24 @@ let apiData = document.getElementById('marsApiRoverData')
 apiData.value = roverId;
 document.getElementById('frmRoverType').submit()
 }))
+
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
+const parameter = getUrlParameter("marsApiRoverData");
+highLightButton(parameter)
+
+if(parameter =='')
+{
+parameter = 'marsApi';
+}
+
+function highLightButton(roverType)
+{
+    document.getElementById('marsApi'+roverType).classList.remove('btn-secondary')
+    document.getElementById('marsApi'+roverType).classList.add('btn-primary')
+}
